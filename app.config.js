695597c0984968736c81efc541a6de2b59f3d825
Amrit <amritpal.alampur@gmail.com>
2025-10-12 21:00:51 +0200

    changed version on app.json.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "FermiToday",
     slug: "fermi-today",
-    version: "0.1.0",
+    version: "0.2.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
