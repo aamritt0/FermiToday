4599244a116f9730ce489fce4c2ebc2f7eaf782a
Amrit <amritpal.alampur@gmail.com>
2025-10-26 10:12:19 +0100

    Add google-services.json via EAS secret

--- eas-build-pre-install.sh
+++ eas-build-pre-install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ -f "$GOOGLE_SERVICES_JSON" ]; then
-  echo "Copying google-services.json to android/app/"
+if [ -n "$GOOGLE_SERVICES_JSON" ]; then
+  echo "Creating google-services.json from base64..."
   mkdir -p android/app
-  cp "$GOOGLE_SERVICES_JSON" android/app/google-services.json
+  echo "$GOOGLE_SERVICES_JSON" | base64 -d > android/app/google-services.json
   echo "Done!"
 else
-  echo "ERROR: GOOGLE_SERVICES_JSON file not found!"
+  echo "ERROR: GOOGLE_SERVICES_JSON not found!"
   exit 1
 fi
